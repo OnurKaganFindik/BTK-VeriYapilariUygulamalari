6b4113ca8788e32b291d524ae50b61b630c15899
ONUR KAGAN FINDIK <onurkagan.findik20@ogr.atauni.edu.tr>
2021-06-09 23:31:13 +0300

    Queue uygulaması eklendi.

---
+++ QueueUygulamasi/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QueueUygulamasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sesliHarfler = new List<char>()
+            {
+                'a','e','ı','i','u','ü','o','ö',
+            };
+            ConsoleKeyInfo secim;
+            var kuyruk = new Queue<char>();
+
+            foreach (char k in sesliHarfler)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"{k,-5 } kuyruk eklensin mi?");
+                secim = Console.ReadKey();
+                Console.WriteLine();
+                if (secim.Key==ConsoleKey.E)
+                {
+                    kuyruk.Enqueue(k);
+                    Console.WriteLine($"\n{k,-5}kuyruğa eklendi.");
+                    Console.WriteLine($"Kuyruktaki eleman sayısı : {kuyruk.Count}");
+                    Console.WriteLine();
+
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
